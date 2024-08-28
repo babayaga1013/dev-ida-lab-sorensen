e3a11e8a80b4a903f70522c4fe8a673e91d5cf2c
babayaga1013 <kalani.sorensen@gmail.com>
2024-08-28 17:52:40 -0500

    app.js uodate

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.get('/', function (req, res) {
+
 app.use(express.static(path.join('./')));
 
-});
+
 app.listen(3000, () => {
   
 });
