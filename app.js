3c3a04521db35b03d3335603d2bde9e4fa103799
babayaga1013 <kalani.sorensen@gmail.com>
2024-08-28 17:58:20 -0500

    app.js fix

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
-const express = require('express');
-const path = require('path');
-const app = express();
+const express = require('express')
+const app = express()
+
+console.log('im on a node server');
+
+app.get('/', function (req, res) {
+  // res.send('Hello Node from Ex on local dev box')
+  res.sendFile(path.join('/index.html'));
+})
+
+app.listen(3000)
 
 
-app.use(express.static(path.join('./')));
 
 
-app.listen(3000, () => {
+// const express = require('express');
+// const path = require('path');
+// const app = express();
+
+
+// app.use(express.static(path.join('./')));
+
+
+// app.listen(3000, () => {
   
-});
+// });
