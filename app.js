02fffd23f3816a1757c34f0f5a49bb1ac151b89a
babayaga1013 <kalani.sorensen@gmail.com>
2024-08-28 17:18:44 -0500

    app.js fix

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 console.log('im on a node server, yo');
 
 
-const express = require('express')
-const app = express()
+const express = require('express');
+const path = require("path");
+const app = express();
+
 
 app.get('/', function (req, res) {
   app.use(express.static('./'))
   // res.send('Hello Node from Ex on local dev box')
-})
+});
 
-app.listen(3000)
+app.listen(3000);
