15cb7f039cef93d96987b9f0d28ed5c4aa13125d
babayaga1013 <kalani.sorensen@gmail.com>
2024-08-28 17:43:08 -0500

    app.js update

--- app.js
+++ app.js
@@ -1,19 +1,3 @@
-// console.log('im on a node server, yo');
-
-
-// const express = require('express');
-// const path = require("path");
-// const app = express();
-
-
-// app.get('/', function (req, res) {
-//   app.use(express.static(path.join('./')));
-// res.send('Hello Node from Ex on local dev box')
-  
-// });
-
- // app.listen(3000);
-
 const express = require('express');
 const path = require('path');
 const app = express();
